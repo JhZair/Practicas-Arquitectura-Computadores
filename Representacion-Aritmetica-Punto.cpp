014517bb592514146d264bd645f88316bf093267
JhZair <98540197+JhZair@users.noreply.github.com>
2025-04-09 18:02:50 -0500

    Avance en división de punto flotante

---
+++ Representacion-Aritmetica-Punto.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+	float divisionFp(float div1, float div2){
+		if (div1 == 0){
+			return 0;
+		}
+		else if(div2 == 0){
+			return 0;
+		}
+		
+	}
+
+
+	int main(){
+
+		float dividendo, divisor;
+		
+		cout << "Ingresa el dividendo: "<<endl;
+		cin >> dividendo;
+		
+		cout << "Ingresa el divisor: " << endl;
+		cin >> divisor;
+		
+		cout << divisionFp(dividendo, divisor) << endl;
+		
+		return 0;
+	}
+
+
+	//g++ -o booth_dinamico Arq.cpp
